c6f83fb9f42fb241640f7350100c9388be8a3d32
Steven <stevenpstansberry@gmail.com>
2025-02-17 16:50:45 -0800

    Edited dependencies

--- book-reader/next.config.ts
+++ book-reader/next.config.ts
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Ignore ESLint errors during the build process
+  },
+  webpack: (config: { resolve: { fallback: any; }; }, { isServer }: any) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false, // Prevent Webpack from bundling canvas
+      };
+    }
+    return config;
   },
 };
 
